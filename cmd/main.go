b6db264c9b86bca8f639d4d32dcdb29e46fa08e4
Denis Evgrafov <devgrafov@cinimex.ru>
2025-08-21 18:18:37 +0300

    Настроено подключение к БД

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"idm/database"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello, Go.")
+	db := database.ConnectDb()
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+
+	fmt.Println("Connected to database")
+
+	defer db.Close()
 }
